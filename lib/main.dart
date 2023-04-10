6610a648fe5153e8df0f9a93bb59bed7dea38ceb
MohamedSalah <elkomymohamed69@gmail.com>
2023-04-10 11:10:16 +0200

    build structure

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'Features/Splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplashView(),
+
+    );
+  }
+}
+

103d19c2516e86d7c3c95b14312888a20a5cd58b
MohamedSalah <elkomymohamed69@gmail.com>
2023-04-10 16:16:04 +0200

    adding sliding animation to splash body

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,18 +1,45 @@
+import 'package:bookly/Features/Splash/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class SplahViewBody extends StatelessWidget {
+class SplahViewBody extends StatefulWidget {
   const SplahViewBody({super.key});
 
   @override
+  State<SplahViewBody> createState() => _SplahViewBodyState();
+}
+
+class _SplahViewBodyState extends State<SplahViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children:  [
-         Image.asset(AssetData.logo)    
+      children: [
+        Image.asset(AssetData.logo),
+        const SizedBox(
+          height: 4,
+        ),
+        SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }

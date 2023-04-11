baf4de3f197665e870e368c9534c57f0b16f93ef
MohamedSalah <elkomymohamed69@gmail.com>
2023-04-11 13:34:32 +0200

    ..

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly/Features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/featured_list_view_item.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/featured_list_list_view.dart';
 import 'package:flutter/material.dart';
 

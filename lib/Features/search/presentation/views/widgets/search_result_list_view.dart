59de4747ee072c348b9172c225d41d5af3889733
MohamedSalah <elkomymohamed69@gmail.com>
2023-04-18 13:54:51 +0200

    create search view & ui done

--- lib/Features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/Features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,15 +1,16 @@
-import 'package:bookly/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:flutter/material.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+import '../../../../home/presentation/views/widgets/best_seller_list_view_item.dart';
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      physics: const NeverScrollableScrollPhysics(),
+      
       padding: EdgeInsets.zero,
-      itemCount: 5,
+      itemCount: 10,
       itemBuilder: (context, index) {
         return const Padding(
           padding: EdgeInsets.symmetric(vertical: 10),
@@ -17,5 +18,6 @@ class BestSellerListView extends StatelessWidget {
         );
       },
     );
+    
   }
 }
--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:bookly/Features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:bookly/Features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -9,12 +12,27 @@ class SearchViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: const [
+          SizedBox(
+            height: 16,
+          ),
           CustomSearchTextField(),
+          SizedBox(
+            height: 32,
+          ),
+          Text(
+            'Result',
+            style: Styles.textStyle20,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Expanded(
+            child: SearchResultListView(),
+          ),
         ],
       ),
     );
   }
 }
-
-

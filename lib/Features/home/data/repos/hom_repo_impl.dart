76325f7800949bc981adb5ec6c15d9bef210d55c
MohamedSalah <elkomymohamed69@gmail.com>
2023-04-26 12:09:39 +0200

    create fetchFeathuredBooks method

--- lib/Features/home/data/repos/hom_repo_impl.dart
+++ lib/Features/home/data/repos/hom_repo_impl.dart
@@ -13,7 +13,7 @@ class HomeRepoImpl implements HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
     try {
       var data = await apiService.get(
-          endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+          endPoint: 'volumes?Filtering=free-ebooks&Sorting:newest &q=subject:programming');
       List<BookModel> books = [];
       for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
@@ -29,8 +29,21 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeathuredBooks() {
-    // TODO: implement fetchFeathuredBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeathuredBooks() async{
+   try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
   }
 }
